a7985bbca537a7271ae757e8ab120194a3bad579
Gideon Ukoko Jr <gideon.ukoko98@gmail.com>
2023-05-31 17:13:57 +0100

    Initial commit

---
+++ src/main/java/com/gideon/jrrpncalculator/GideonUkokoJrRpnCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.gideon.jrrpncalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GideonUkokoJrRpnCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GideonUkokoJrRpnCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gideon/jrrpncalculator/GideonUkokoJrRpnCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gideon.jrrpncalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GideonUkokoJrRpnCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
